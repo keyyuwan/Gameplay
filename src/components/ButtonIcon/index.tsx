de15398efff644129429dea985235c974f2f78b7
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-09-30 21:57:44 -0300

    [day 02] svg config + ButtonAdd + CategorySelect

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,27 +1,24 @@
 import React from "react";
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  TouchableOpacityProps,
-} from "react-native";
+// RectButton lida melhor com a experiencia do usuário de acordo com a plataforma
+// IOS e Android
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { View, Text, Image } from "react-native";
 
 import DiscordImg from "../../assets/discord.png";
 import { styles } from "./style";
 
-type ButtonIconProps = TouchableOpacityProps & {
+type ButtonIconProps = RectButtonProps & {
   title?: string;
 };
 
 export function ButtonIcon({ title, ...rest }: ButtonIconProps) {
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <RectButton style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
 
       <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 
 import { Profile } from "../../components/Profile";
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
 
 import { styles } from "./style";
 
 export function Home() {
+  const [category, setCategory] = useState("");
+
+  function handleSelectCategory(categoryId: string) {
+    categoryId === category ? setCategory("") : setCategory(categoryId);
+  }
+
   return (
     <View>
       <View style={styles.header}>
         <Profile />
+        <ButtonAdd />
+      </View>
+
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          handleSelectCategory={handleSelectCategory}
+        />
       </View>
     </View>
   );
